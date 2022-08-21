068a0a3ef2834c04c904bc3905d5f5b60a5c54b8
ankush <mehraankush36@gmail.com>
2022-08-21 17:34:35 +0530

    more functions

--- basic/string/string1.js
+++ basic/string/string1.js
@@ -18,3 +18,16 @@ console.log(result3);
 
 const result4 = text1.slice(1,5);
 console.log(result4);
+
+// charAt(index)	returns the character at the specified index
+// concat()	        joins two or more strings
+// replace()	    replaces a string with another string
+// split()	        converts the string to an array of strings
+// substr(start, length)	returns a part of a string
+// substring(start,end)	    returns a part of a string
+// slice(start, end)	    returns a part of a string
+// toLowerCase()	        returns the passed string in lower case
+// toUpperCase()	        returns the passed string in upper case
+// trim()	                removes whitespace from the strings
+// includes()	            searches for a string and returns a boolean value
+// search()             	searches for a string and returns a position of a match
