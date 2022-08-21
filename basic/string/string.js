28073685cb3f301b74f2de01a20da55e99fcc689
ankush <mehraankush36@gmail.com>
2022-08-21 17:32:22 +0530

    string-functions

---
+++ basic/string/string.js
@@ -0,0 +1,8 @@
+let string = 'Delta compression using up to 8 threads \
+Compressing objects: 100% (5/5), done.'
+
+let string1 = 'Delta compression using up to 8 threads' +
+'Compressing objects: 100% (5/5), done.'
+
+console.log(string.length);
+console.log(string1.length);
---
+++ basic/string/string1.js
@@ -0,0 +1,20 @@
+const text1 ='hello';
+const text2 ='world';
+const text3 ='   javascript   ';
+
+const result = text1.toUpperCase();
+console.log(result);
+
+const result1 = text1.concat(' ',text2);
+console.log(result1);
+
+const result2 = text3.trim();
+console.log(result2);
+
+
+const result3 = text1.split();
+console.log(result3);
+
+
+const result4 = text1.slice(1,5);
+console.log(result4);
