34e50edc49fbd9f063247c6b0437037ffd17c9f1
ankush <mehraankush36@gmail.com>
2022-08-23 17:17:31 +0530

    more functions

---
+++ basic/infinity.js
@@ -0,0 +1,11 @@
+const c = 88888787878n;  // syntax of big int
+const d = c + 1n;  // we can add only bigint in bigint not just number
+console.log(d); //88888787879n
+
+
+const a =2/0;
+console.log(a);
+
+
+const b = -2/0;
+console.log(b);
---
+++ basic/isNaN.js
@@ -0,0 +1,7 @@
+const a = '4'-'hello';
+console.log(a); //NaN
+console.log(typeof a); //number
+
+const b = isNaN(a)
+console.log(b); //true
+
