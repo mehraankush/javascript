c2ec67deb11a8fb507420ba5c01af8cc1f3b4ba1
ankush <mehraankush36@gmail.com>
2022-08-26 21:52:25 +0530

    Error handling

---
+++ basic/debugging/debugger.js
@@ -0,0 +1,12 @@
+let a = 6;
+let b = 9;
+let c = a * b;
+
+debugger;
+console.log(a);
+// stops the execution
+debugger;
+//The debugger keyword stops the execution of the code and calls the debugging function.
+//you can resume the code after examine it 
+
+console.log(c);
