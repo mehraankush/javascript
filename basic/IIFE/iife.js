b2cede64774610d52d3a8cac416a7dc86f15c331
ankush <mehraankush36@gmail.com>
2022-08-26 21:52:58 +0530

    IIFE

---
+++ basic/IIFE/iife.js
@@ -0,0 +1,8 @@
+//imwdeatly invoked function  expretion
+//IIFE allow us not to five function a name we can can use then without a name
+(
+    function(){
+        let a =90;
+        console.log(a);
+    }
+)();
