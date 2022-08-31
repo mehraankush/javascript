2d19bf765643880fcb01acfb9defe16e4f78f261
ankush <mehraankush36@gmail.com>
2022-08-31 23:21:11 +0530

    nested terniary

---
+++ basic/nested-ter.js
@@ -0,0 +1,8 @@
+function randomnum(num){
+    
+    return (num>0) ? "positive" : (num<0) ? "negative" : "zero" ;
+}
+
+console.log(randomnum(0));
+
+//object.freeze(nameoffunction/nameofObject)
---
+++ basic/obj-return.js
@@ -0,0 +1,8 @@
+
+let obj = (name,age,gender) =>({name,age,gender});
+
+console.log(obj('mickey',69,'male'));
+
+//import everything from a file 
+//import * as objact_name from file_name
+//object_name could be anything
