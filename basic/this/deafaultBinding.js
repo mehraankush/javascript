c3e5724aa14635333b48d62881b9c9e6ff9a17ad
ankush <mehraankush36@gmail.com>
2023-03-02 22:17:15 +0530

    Default bonding in this keyword

---
+++ basic/this/deafaultBinding.js
@@ -0,0 +1,16 @@
+
+
+// if this.name is not 
+globalThis.name = "viswas"
+function Person(name){
+    console.log(`My name is ${this.name}`);
+}
+
+Person()
+
+// flow of the all types of this useages
+
+// New bonding
+// Explicit bonding
+// Inplicit bonding
+// default bonding
